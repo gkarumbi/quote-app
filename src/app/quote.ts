70f27509dfbbf5cb600021ea02d86251bcbc158e
gkarumbi <gkarumbi@gmail.com>
2020-06-16 20:04:52 +0300

    Create quote class to handle all our data

---
+++ src/app/quote.ts
@@ -0,0 +1,10 @@
+export class Quote {
+
+    author:string;
+    quote:string;
+    sumbmittedBy:string;
+    
+    /* constructor(public author:string, public quote:string, public submittedBy:string){
+
+    } */
+}
