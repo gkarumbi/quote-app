b2ca53d3afb8c560f5f21b6c16cf40f027c3bb4e
gkarumbi <gkarumbi@gmail.com>
2020-06-17 08:58:11 +0300

    Add function quoteDelete to delete the quote

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.scss']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+   new Quote("William Shakespeare", "To be or not to be is the question","George Karumbi"),
+    new Quote("Chinua Achebe","Until the lion tells his side of the story, the tale of the hunt will always glorify the hunter","Liz Nderitu"),
+   new Quote ("George .W. Bush", "Fool me once, shame on you fool me twice...well if you get fooled you can't get fooled again","Mjuaji OO1")
+  ];
+
+  toggleDetails(index){
+    this.quotes[index].showMore = !this.quotes[index].showMore;
+
+  }
+  quoteDelete(isRead, index){
+    if(isRead){
+      this.quotes.splice(index,1);
+
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
