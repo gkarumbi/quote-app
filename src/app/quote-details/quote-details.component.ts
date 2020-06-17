adbb450505317c57761209318651d55bf262fb89
gkarumbi <gkarumbi@gmail.com>
2020-06-17 08:34:20 +0300

    Add event emitter to quote details component

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.scss']
+})
+export class QuoteDetailsComponent implements OnInit {
+
+  @Input() quote: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+  quoteRead(complete:boolean){
+    this.isRead.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
